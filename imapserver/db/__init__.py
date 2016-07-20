fc96738f8749d8cc64fa946578a6c19aa5f2f4e1
Eugene Obukhov <irvind25@gmail.com>
2016-07-20 05:27:31 +0300

    Add tests for DbLayer class

--- imapserver/db/__init__.py
+++ imapserver/db/__init__.py
@@ -2,6 +2,12 @@ class DbLayer(object):
     def __init__(self, model_name=None):
         self.model_name = model_name
 
+    def select(self, **kwargs):
+        pass
+
+    def insert(self, **kwargs):
+        pass
+
 
 Message = DbLayer('message')
 Mailbox = DbLayer('mailbox')
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,32 @@
-from imapserver import db
+import pytest
+
+from imapserver.db import DbLayer
 
 
+def test_single_select():
+    layer = DbLayer('cat')
+    row = layer.select(name='Jinger')[0]
+    assert row['name'] == 'Jinger'
+
+
+@pytest.mark.skip
+def test_select_order_by():
+    # Jinger 5
+    # Kleo 7
+    # Joseph 8
+    layer = DbLayer('cat')
+    rows = layer.select(age=('>', 5), order_by=['age'])
+
+    assert rows[0]['name'] == 'Jinger'
+    assert rows[1]['name'] == 'Kleo'
+    assert rows[2]['name'] == 'Joseph'
+
+
+@pytest.mark.skip
+def test_select_order_by_desc():
+    layer = DbLayer('cat')
+    rows = layer.select(age=('>', 5), order_by=['-age'])
+
+    assert rows[2]['name'] == 'Joseph'
+    assert rows[1]['name'] == 'Kleo'
+    assert rows[0]['name'] == 'Jinger'
